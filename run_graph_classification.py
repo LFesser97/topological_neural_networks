5dd0d2c174ca48afe4ca52548da3630b8a2afceb
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-07-30 10:32:09 -0400

    removed mutag

--- run_graph_classification.py
+++ run_graph_classification.py
@@ -32,7 +32,7 @@ imdb = list(TUDataset(root="data", name="IMDB-BINARY"))
 #with open("imdb.pkl", "rb") as f:
 #    imdb = pickle.load(f)
 
-datasets = {"mutag" : mutag, "enzymes" : enzymes, "imdb": imdb, "proteins": proteins}
+datasets = {"enzymes" : enzymes, "imdb": imdb, "proteins": proteins}
 
 
 for key in datasets:
