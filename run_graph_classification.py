6ab619a8aa8fc848f5f44b8df62dc717e86c855b
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-07-31 20:38:26 -0400

    only imdb

--- run_graph_classification.py
+++ run_graph_classification.py
@@ -34,7 +34,7 @@ imdb = list(TUDataset(root="data", name="IMDB-BINARY"))
 
 # datasets = {"mutag": mutag, "enzymes" : enzymes, "imdb": imdb, "proteins": proteins}
 
-datasets = {"enzymes": enzymes}
+datasets = {"imdb": imdb}
 
 for key in datasets:
     if key in ["reddit", "imdb", "collab"]:
