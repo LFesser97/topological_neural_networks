08b5371f2bfd62798c71e1ed34ce949732126b65
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-08-03 13:12:57 -0400

    proteins only

--- run_graph_classification.py
+++ run_graph_classification.py
@@ -32,7 +32,7 @@ imdb = list(TUDataset(root="data", name="IMDB-BINARY"))
 #with open("imdb.pkl", "rb") as f:
 #    imdb = pickle.load(f)
 
-datasets = {"imdb": imdb}
+datasets = {"proteins": proteins}
 
 # datasets = {"mutag": mutag}
 
