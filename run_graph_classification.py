265198007d7eaf5f393358dec7b48bc0bb3a1b4b
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-08-04 10:16:21 -0400

    enzymes only

--- run_graph_classification.py
+++ run_graph_classification.py
@@ -34,7 +34,7 @@ imdb = list(TUDataset(root="data", name="IMDB-BINARY"))
 
 # datasets = {"mutag": mutag, "enzymes": enzymes, "proteins": proteins, "imdb": imdb}
 
-datasets = {"proteins": proteins}
+datasets = {"enzymes": enzymes}
 
 
 for key in datasets:
