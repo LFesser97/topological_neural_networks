82e8f292f6f602c0b4d8f7c92f5658d50e5321c7
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-08-11 17:34:32 -0400

    debugging

--- scripts/run_node_sdrf.sh
+++ scripts/run_node_sdrf.sh
@@ -1,9 +1,9 @@
-python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 12 --dataset cora  --sdrf_remove_edges
-python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 175 --dataset citeseer
-python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 87 --dataset texas --sdrf_remove_edges
-python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 100 --dataset cornell --sdrf_remove_edges
-python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 25 --dataset wisconsin --sdrf_remove_edges
-python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 50 --dataset chameleon --sdrf_remove_edges
+python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 12 --dataset cora  --sdrf_remove_edges True
+python run_node_classification.py     # --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 175 --dataset citeseer --sdrf_remove_edges 
+python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 87 --dataset texas --sdrf_remove_edges True
+python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 100 --dataset cornell --sdrf_remove_edges True
+python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 25 --dataset wisconsin --sdrf_remove_edges True
+python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GCN    --num_trials 100    --device cuda:0     --num_iterations 50 --dataset chameleon --sdrf_remove_edges True
 
 #python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GIN    --num_trials 100    --device cuda:0     --num_iterations 50 --dataset cora
 #python run_node_classification.py     --rewiring sdrf_bfc     --layer_type GIN    --num_trials 100    --device cuda:0     --num_iterations 25 --dataset citeseer
