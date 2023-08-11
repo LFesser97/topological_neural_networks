a0bc7b648805dd52a06374c8ad2fff7ebd06ed91
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-08-11 15:43:05 -0400

    gcn fosr and sdrf only

--- scripts/run_graph_fosr.sh
+++ scripts/run_graph_fosr.sh
@@ -3,7 +3,7 @@ python run_graph_classification.py     --rewiring fosr     --layer_type GCN    -
 python run_graph_classification.py     --rewiring fosr     --layer_type GCN    --num_trials 100    --device cuda:0    --num_iterations 10 --dataset mutag
 python run_graph_classification.py     --rewiring fosr     --layer_type GCN    --num_trials 100    --device cuda:0    --num_iterations 30 --dataset proteins
 
-python run_graph_classification.py     --rewiring fosr     --layer_type GIN    --num_trials 100    --device cuda:0    --num_iterations 5 --dataset enzymes
-python run_graph_classification.py     --rewiring fosr     --layer_type GIN    --num_trials 100    --device cuda:0    --num_iterations 20 --dataset imdb
-python run_graph_classification.py     --rewiring fosr     --layer_type GIN    --num_trials 100    --device cuda:0    --num_iterations 20 --dataset mutag
-python run_graph_classification.py     --rewiring fosr     --layer_type GIN    --num_trials 100    --device cuda:0    --num_iterations 10 --dataset proteins
+#python run_graph_classification.py     --rewiring fosr     --layer_type GIN    --num_trials 100    --device cuda:0    --num_iterations 5 --dataset enzymes
+#python run_graph_classification.py     --rewiring fosr     --layer_type GIN    --num_trials 100    --device cuda:0    --num_iterations 20 --dataset imdb
+#python run_graph_classification.py     --rewiring fosr     --layer_type GIN    --num_trials 100    --device cuda:0    --num_iterations 20 --dataset mutag
+#python run_graph_classification.py     --rewiring fosr     --layer_type GIN    --num_trials 100    --device cuda:0    --num_iterations 10 --dataset proteins
