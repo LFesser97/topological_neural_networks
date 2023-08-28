4ab10592f0a3a79b3b72923c18ede77baa867f9a
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-08-28 10:51:59 -0400

    debugging

--- experiments/graph_classification.py
+++ experiments/graph_classification.py
@@ -28,7 +28,7 @@ default_args = AttrDict(
     "output_dim": 1,
     "hidden_layers": None,
     "num_layers": 1,
-    "batch_size": 64,
+    "batch_size": 10,
     "layer_type": "R-GCN",
     "num_relations": 2,
     "last_layer_fa": False
@@ -169,6 +169,7 @@ class Experiment:
                 total_correct += pred.eq(y).sum().item()
                 
         return total_correct / sample_size
+    
     def check_dirichlet(self, loader):
         self.model.eval()
         sample_size = len(loader.dataset)
