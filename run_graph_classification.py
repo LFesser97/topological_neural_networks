25cfa7161650069701fb61742fd6fc5c816ce690
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-09-17 17:46:23 -0400

    LCP + LAPE

--- run_graph_classification.py
+++ run_graph_classification.py
@@ -190,8 +190,8 @@ for key in datasets:
         # transform = T.AddRandomWalkPE(walk_length=16)
         # print("Encoding Random Walk PE")
 
-        # transform = T.AddLaplacianEigenvectorPE(k=8)
-        # print("Encoding Laplacian Eigenvector PE")
+        transform = T.AddLaplacianEigenvectorPE(k=8)
+        print("Encoding Laplacian Eigenvector PE")
 
         # transform = T.RootedRWSubgraph(walk_length=10)
         # print("Encoding Rooted RW Subgraph")
@@ -207,10 +207,10 @@ for key in datasets:
 
         try:
             lcp = LocalCurvatureProfile()
-            print(f"Encoding Local Curvature Profile (AFRC-4) for graph {current_graph} of {org_dataset_len}")
+            print(f"Encoding Local Curvature Profile (ORC) for graph {current_graph} of {org_dataset_len}")
 
-            dataset[i] = lcp.compute_afrc_4(dataset[i])
-            # dataset[i] = transform(dataset[i])
+            dataset[i] = lcp.compute_orc(dataset[i])
+            dataset[i] = transform(dataset[i])
 
             current_graph += 1
 
