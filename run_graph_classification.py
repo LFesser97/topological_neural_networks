69f3d07469fbccdeff8b222d6b03e42e00ae849a
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-09-16 21:04:41 -0400

    debugging

--- run_graph_classification.py
+++ run_graph_classification.py
@@ -126,7 +126,7 @@ default_args = AttrDict({
     "eval_every": 1,
     "rewiring": "borf",
     "num_iterations": 3,
-    "patience": 100,
+    "patience": 30,
     "output_dim": 2,
     "alpha": 0.1,
     "eps": 0.001,
