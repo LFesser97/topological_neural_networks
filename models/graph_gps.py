b348392349037adc8324c074e239aa56bbb49785
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-10-21 14:59:55 -0400

    debugging

--- models/graph_gps.py
+++ models/graph_gps.py
@@ -95,6 +95,10 @@ class GPS(torch.nn.Module):
 
     def forward(self, x, pe, edge_index, edge_attr, batch):
         x_pe = self.pe_norm(pe)
+        
+        print(self.node_emb(x.squeeze(-1).long()).shape)
+        print(self.pe_lin(x_pe).shape)
+
         x = torch.cat((self.node_emb(x.squeeze(-1).long()), self.pe_lin(x_pe)), 1)
         edge_attr = self.edge_emb(edge_attr)
 
