aaf85c4ff011f7afe96a5799f8818fe86df73bd3
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-10-21 13:51:45 -0400

    debugging

--- models/graph_gps.py
+++ models/graph_gps.py
@@ -29,7 +29,7 @@ transform = T.AddRandomWalkPE(walk_length=20, attr_name='pe')
 
 """ Graph Classification Datasets """
 
-mutag = list(TUDataset(root="data", name="MUTAG", pre_transform=transform))
+mutag = list(TUDataset(root="data", name="MUTAG", transform=transform))
 # enzymes = list(TUDataset(root="data", name="ENZYMES"))
 # proteins = list(TUDataset(root="data", name="PROTEINS"))
 # imdb = list(TUDataset(root="data", name="IMDB-BINARY"))
