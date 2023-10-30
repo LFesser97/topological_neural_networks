1979033cd76c6d2cc68519328641040bf251c74a
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-10-30 16:57:41 -0400

    LAPE + SUB

--- models/graph_gps.py
+++ models/graph_gps.py
@@ -27,8 +27,14 @@ from attention import PerformerAttention
 
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'ZINC-PE')
-transform = T.AddRandomWalkPE(walk_length=20, attr_name='pe')
+# transform = T.AddRandomWalkPE(walk_length=20, attr_name='pe')
 # transform = T.AddLaplacianEigenvectorPE(k=8, attr_name='pe')
+# transform = T.Compose([T.RootedRWSubgraph(walk_length=10), T.AddRandomWalkPE(walk_length=16)])
+# print("Encoding Rooted RW Subgraph + Random Walk PE")
+
+transform = T.Compose([T.RootedRWSubgraph(walk_length=10), T.AddLaplacianEigenvectorPE(k=8)])
+print("Encoding Rooted RW Subgraph + Laplacian Eigenvector PE")
+
 
 train_dataset = ZINC(path, subset=True, split='train', pre_transform=transform)
 val_dataset = ZINC(path, subset=True, split='val', pre_transform=transform)
